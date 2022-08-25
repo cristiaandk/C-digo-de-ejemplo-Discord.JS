59b67e4a8341b3d4f68f697fc8836b4e81f35040
cristiaandk <112103088+cristiaandk@users.noreply.github.com>
2022-08-25 18:21:51 +0200

    Update index.js

--- ejemplo-v13/index.js
+++ ejemplo-v13/index.js
@@ -1,3 +1,5 @@
+// PARA INSTALAR DISCORD.JS V13: yarn/npm/pnpm i discord.js@13 --s
+
 const Discord = require("discord.js");
 const client = new Discord.Client({
 	intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES]
